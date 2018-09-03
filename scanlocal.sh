c8d82ba6b1d24e114ae5a1259ea3f2a1b45a85c1
mrdavesampson <ds666@protonmail.com>
2018-09-02 20:00:29 -0400

    Added report options to scanlocal.sh

--- scanlocal.sh
+++ scanlocal.sh
@@ -21,7 +21,7 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 while :
 do
 echo
-read -r -p "Save scan reults in current directory: "$SCRIPT_DIR"? [Y/n]" response
+read -r -p "Save scan reults in current directory: "$SCRIPT_DIR"? [Y/n]  " response
 response=${response,,}
 if [[ $response =~ ^(yes|y)$ ]]
 then 
@@ -30,15 +30,18 @@ then
    echo "Scan results will be saved in "$DIR"" && break
 else
    echo
-   echo "Enter path to directory where results should be saved"; echo
-   read DIR
+   echo "Enter path to directory where results should be saved"
+   echo
+   read RDIR
+   #convert relative path to absolute path
+   DIR=$(readlink -f ${RDIR})
    if [ -d "${DIR}" ] 
    then
         echo
         echo "Scan results will be saved in "$DIR"" && break
    else
         echo
-        read -r -p ""$DIR" does not exist; would you like to create it? [y/N]" response
+        read -r -p ""$DIR" does not exist; would you like to create it? [y/N]  " response
         response=${response,,}
         if [[ $response =~ ^(yes|y)$ ]]
         then 
@@ -69,11 +72,9 @@ for DEV in `ip addr | awk '/state UP/ {print $2}' | sed 's/.$//'`; do
 ((i++))
 ip -o -c addr show | grep -w $DEV | grep -w inet | awk '{printf "%-10s %s\n", $2, $4}'
 done
-if i=1
+
+if [ $i -ne 1 ]
 then
-    echo
-    echo "Only one active interface found; defaulting to "$DEV""
-else
     echo
     echo "Please select the network interface corresponding to the local subnet you want to scan (e.g. "wlan0" or "eth0"):"
     echo
@@ -82,20 +83,19 @@ else
     read DEV
     echo "You have chosen interface "$DEV""
 fi
+
 # set variables for local subnet based on device chosen
 IPADDR="$(ifconfig $DEV | awk '$1 == "inet" {print $2}')"
 MASK="$(ifconfig $DEV | awk '$1 == "inet" {print $4}')"  
 echo
-echo "Local IP Address is "${IPADDR}""
-echo 
-echo "Local Subnet mask is "${MASK}""
-echo
+echo "Local IP Address:   "${IPADDR}"" 
+echo "Local Subnet mask:  "${MASK}""
 
 if [[ $MASK = "255.255.255.0" ]]
 then 
     NETWORK="$(ip -o addr show dev "$DEV" | awk '$3 == "inet" {print $4}' | cut -f1-3 -d ".")"
     SUBNET="${NETWORK}.0/24"
-    echo "Local Subnet in CIDR Notation is "${SUBNET}""
+    echo "Local Subnet:       "${SUBNET}""
     echo
 else 
     echo "This is not a /24 Subnet"
@@ -147,7 +147,7 @@ case $OPTION in
         echo
         read PORT
         echo
-        read -r -p "Scan "$SUBNET" subnet for port(s) "$PORT"; Is this correct? [y/N]" response
+        read -r -p "Scan "$SUBNET" subnet for port(s) "$PORT"; Is this correct? [y/N]  " response
         response=${response,,}
         if [[ $response =~ ^(yes|y)$ ]] 
         then 
@@ -159,7 +159,7 @@ case $OPTION in
 
     [aA] )
         echo 
-        read -r -p "Perform Full Scan of "$SUBNET"; Is this correct? [y/N]" response
+        read -r -p "Perform Full Scan of "$SUBNET"; Is this correct? [y/N]  " response
         response=${response,,}
         if [[ $response =~ ^(yes|y)$ ]] 
         then 
@@ -171,12 +171,14 @@ case $OPTION in
 
     [sS] )
         echo 
-        read -r -p "Perform Ping Scan of "$SUBNET"; Is this correct? [y/N]" response
+        read -r -p "Perform Ping Scan of "$SUBNET"; Is this correct? [y/N]  " response
         response=${response,,}
         if [[ $response =~ ^(yes|y)$ ]] 
         then 
             OPTS="-sn"
-            jumpto SCAN
+           # REP=""
+           # FN=""
+           # jumpto SCAN
         else
             jumpto $START
         fi    
@@ -206,7 +208,7 @@ case $OPTION in
     [qQ] ) 
         echo
         echo
-        exit 
+        exit 0
         ;;
 
     *) echo "Invalid Scan Option ..."
@@ -215,41 +217,143 @@ case $OPTION in
     ;;
 esac
 echo
-echo
-read -r -p "Use Stealth Mode? [y/N]" stealth
-stealth=${stealth,,}
-if [[ $stealth =~ ^(yes|y)$ ]]
-then
-    OPTS="${OPTS} -sS"
+# prompt for stealth mode unless ping scan
+if [[ $OPTS="-sn" ]]
+then OPTS="-sn"
+else    
+    read -r -p "Use Stealth Mode? [y/N]" stealth
+    stealth=${stealth,,}
+    if [[ $stealth =~ ^(yes|y)$ ]]
+    then
+        OPTS="${OPTS} -sS"
+    fi   
 fi    
-
+echo
+echo "Select report option:"
+while true
+do
+echo
+echo "   N: -oN Normal NMAP Output; no parsing will be done"
+echo "   O: -oG List IPs with Open Ports or Online Hosts (valid only for Port & Ping scans)"
+echo "   X: -Ox XML Output"
+echo "   A: -oA All formats: normal, XML, and grepable"
+echo "   I: Interactive only - results will be output to screen but not saved"
+echo "   Q: Quit without scanning"
+echo
+read REPOPT
+case $REPOPT in
+    [nN] ) 
+        REP="-oN"
+        FN=""$DIR"/scanresults-$NETWORK-$OPTS"
+        break
+        ;;
+    [oO] )
+        echo
+        if [[ $OPTION =~ ^(pPsS)$ ]]
+        then 
+            REP="-oG"
+            if [[ $OPTION=~ ^(pP)$ ]]
+            then
+                FN=""$DIR"/scanresults-$NETWORK-port-$PORT"
+            else
+                FN=""$DIR"/scanresults-$NETWORK-online_hosts"
+            fi
+            break
+        else
+            echo "Sorry, -oG is not valid report option for a "$OPS" scan "
+        fi
+        ;;
+    [xX] )
+        echo
+        REP="-oX"
+        FN=""$DIR"/scanresults-$NETWORK-$OPTS.xml"
+        break
+        ;;
+    [aA] )
+        echo
+        REP="-oA"
+        FN=""$DIR"/scanresults-$NETWORK-$OPTS"
+        break
+        ;;
+    [iI] )
+        echo
+        REP=""
+        FN=""
+        break
+        ;;
+    [qQ] )
+        exit 0
+        ;;
+      *)
+        echo
+        echo "Please Enter a Valid Report Option ... "
+        ;;
+esac
+done
+        
 # final confirmation of options and actual nmap scan
 SCAN:
 echo
-echo "Perform Scan of "$SUBNET" with options: "${OPTS}"? [y/N]"
-read response
+read -r -p "Perform Scan of "$SUBNET" with options: "${OPTS}" "${REP}"? [y/N]  " response
 if [[ $response =~ ^(yes|y)$ ]] 
 then 
     echo    
     echo
-    echo "Performing NMAP Scan of "$SUBNET" with options: "${OPTS}" now ..."
+    echo "Performing NMAP Scan of "$SUBNET" with options: "${OPTS}" "${REP}" now ..."
+    echo 
+    nmap ${OPTS} ${SUBNET} ${REP} ${FN}
     echo
-    nmap ${OPTS} ${SUBNET} -oG scanresults
-    cat scanresults |grep open > portsopen
-    cat portsopen |cut -f2 -d ":" |cut -f1 -d"(" > scanresults-$NETWORK-port-$PORT
-    #clean-up temp results files and then display final results
-    echo
-    echo "Cleaning up temp files ...."
-    rm -f scanresults
-    rm -f portsopen
-    echo
-    echo "Showing list of vulnerable ip addresses now ..."
-    echo
-    cat scanresults-$NETWORK-port-$PORT
-    echo
-    echo "Results have been saved to "$DIR"/scanresults-"$NETWORK"-port-"$PORT""
-    echo
-    echo
+    if [[ $REP = "-oG" ]]
+       if [[ $OPTION=~ ^(pP)$ ]] 
+        # Port Scan with Report Option O: Parse OPEN Ports
+        then
+            cat "$FN" | grep open > portsopen
+            cat portsopen |cut -f2 -d ":" |cut -f1 -d"(" > $FN
+            #clean-up temp results files and then display final results
+            echo
+            echo "Cleaning up temp files ... "
+            rm -f scanresults
+            rm -f portsopen
+            echo
+            echo "Showing list of vulnerable ip addresses now ..."
+            echo
+            cat $FN
+            echo
+            echo "Results have been saved to "$FN""
+            echo
+            echo
+        else
+        # Ping Scan with Report Option O: Parse Online Hosts
+            cat "$FN" | grep $NETWORK > hosts
+            cat hosts |cut -f2 -d ":" |cut -f1 -d"(" > $FN
+            #clean-up temp results files and then display final results
+            echo
+            echo "Cleaning up temp files ... "
+            rm -f scanresults
+            rm -f hosts
+            echo
+            echo "Showing list of online hosts now ..."
+            echo
+            cat $FN
+            echo
+            echo "Results have been saved to "$FN""
+            echo
+            echo
+        fi    
+    elif [[ $REP = "" ]]
+    then
+        echo
+        echo "Interactive Scan Complete, Goodbye."
+        echo
+        echo
+        exit 0
+    else
+        echo
+        echo "Results have been saved to "$FN""
+        echo
+        echo
+    fi           
 else
     jumpto $START
 fi    
+exit 0
